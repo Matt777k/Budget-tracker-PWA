a2d12fc7686ce0c7330703ea3fd2f38a2213d4ee
Matthew Kerr <matt777k@gmail.com>
2020-10-19 19:01:34 -0400

    working service worker.May not be working offline

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,57 @@
+console.log("Hi from your service-worker.js file!");
+
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/manifest.webmanifest',
+  '/index.js',
+  '../models/transaction.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
